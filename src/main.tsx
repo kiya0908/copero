2ee2bca9c455749f47560564a12e0f2b40371a4b
kiya0908 <2015189466@qq.com>
2026-08-07 16:32:35 +0800

    refactor(app): mount localized router

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import App from './App.tsx'
-import { NotFoundPage } from './components/pages/NotFoundPage.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from './app/router.tsx'
 import { initAnalytics } from './lib/analytics.ts'
 import './index.css'
+import './styles/tokens.css'
+import './styles/marketing.css'
 
 initAnalytics()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   </StrictMode>,
 )
