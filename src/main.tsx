f9c11b2ff58aded1cd158f923bdbd11d2dcb4357
kiya0908 <2015189466@qq.com>
2026-08-07 16:24:04 +0800

    refactor: add locale-aware application routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import App from './App.tsx'
+import { LocaleLayout } from './app/LocaleLayout.tsx'
 import { NotFoundPage } from './components/pages/NotFoundPage.tsx'
+import { HomePage } from './pages/HomePage.tsx'
 import { initAnalytics } from './lib/analytics.ts'
 import './index.css'
 
@@ -12,8 +14,13 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<Navigate to="/es/" replace />} />
+        <Route path="/:locale" element={<LocaleLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="game" element={<App />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/es/" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
